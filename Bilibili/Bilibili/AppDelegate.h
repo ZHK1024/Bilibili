6101996d6e41b4ae08eaa3bf8e12b3cc840d6d46
389914070 <389914070@qq.com>
2016-09-11 16:44:23 +0800

    create project

---
+++ Bilibili/Bilibili/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Bilibili
+//
+//  Created by Apple-T on 16/9/11.
+//  Copyright © 2016年 Apple-T. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
